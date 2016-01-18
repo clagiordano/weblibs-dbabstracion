d34ab43d599b520ef46460b1e95b71c87a97c5f1
Claudio Giordano <claudio.giordano@autistici.org>
2016-01-19 00:13:56 +0100

    Add DB Interface and first adapter for PDO

---
+++ src/DatabaseAdapterInterface.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace clagiordano\weblibs\dbabstraction;
+
+/**
+ *
+ */
+interface DatabaseAdapterInterface 
+{
+    public function connect();
+    
+    public function disconnect();
+    
+    public function query($queryString);
+    
+    public function fetch();
+    
+    public function select($table, $conditions = '', $fields = '*', $order = '', $limit = null, $offset = null);
+    
+    public function insert($table, array $data);
+    
+    public function update($table, array $data, $conditions);
+    
+    public function delete($table, $conditions);
+    
+    public function getInsertId();
+    
+    public function countRows();
+    
+    public function getAffectedRows();
+}
---
+++ src/PDOAdapter.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace clagiordano\weblibs\dbabstraction;
+
+class PDOAdapter implements DatabaseAdapterInterface 
+{
+    public function __construct()
+    {
+    }
+}
