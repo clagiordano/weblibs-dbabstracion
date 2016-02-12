e6e89af1a5c7cb668f389ffebbae7e929d016239
Claudio Giordano <claudio.giordano@autistici.org>
2016-02-12 18:43:57 +0100

    Fix for standards

--- tests/PDOAdapterTest.php
+++ tests/PDOAdapterTest.php
@@ -7,49 +7,48 @@ use clagiordano\weblibs\dbabstraction\PDOAdapter;
 class PDOAdapterTest extends \PHPUnit_Framework_TestCase
 {
     private $object;
-    
+
     public function setUp()
     {
-        $this->object = new PDOAdapter("localhost", "test", "test", "sample");
-        
+        $this->object = new PDOAdapter('localhost', 'test', 'test', 'sample');
+
         $this->assertInstanceOf(
             'clagiordano\weblibs\dbabstraction\PDOAdapter',
             $this->object
         );
     }
-    
+
     public function testFailedConnection()
     {
-        $this->object = new PDOAdapter("localhosta", "invalid", "invalid", "sample");
-        
+        $this->object = new PDOAdapter('localhosta', 'invalid', 'invalid', 'sample');
+
         $this->assertInstanceOf(
             'clagiordano\weblibs\dbabstraction\PDOAdapter',
             $this->object
         );
-        
+
         $this->setExpectedException('InvalidArgumentException');
         $this->object->connect();
     }
-    
+
     public function testQuery()
     {
-        $resource = $this->object->query("SELECT * FROM tab_sample");
+        $resource = $this->object->query('SELECT * FROM tab_sample');
         $this->assertInstanceOf(
             'PDOStatement',
             $resource
         );
-        
+
         print_r($this->object->fetch());
         print_r($this->object->fetch());
     }
-    
+
     public function testSelect()
     {
-        $resource = $this->object->select("tab_sample");
+        $resource = $this->object->select('tab_sample');
         $this->assertInstanceOf(
             'PDOStatement',
             $resource
         );
-        
     }
 }
