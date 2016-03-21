ce89f066a99fd9a6b26016c2c2c02d11f52a5532
Claudio Giordano <claudio.giordano@autistici.org>
2016-03-21 20:00:39 +0100

    Added sample table for tests

---
+++ testdata/tab_products.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `tab_products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `code` varchar(30) NOT NULL,
+  `brand` varchar(50) NOT NULL,
+  `model` varchar(250) NOT NULL,
+  `description` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='latin1_swedish_ci'
