9eae8a123027a64a3419978d40fc765687d959bb
Claudio Giordano <claudio.giordano@autistici.org>
2017-02-13 18:25:41 +0100

    Add Pager class

---
+++ src/Pager.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace clagiordano\weblibs\dbabstraction;
+
+/**
+ * Response class for output/return formatted JSON data from array
+ *
+ * @package clagiordano\weblibs\dbabstraction
+ */
+class Pager extends PDOAdapter
+{
+    
+}
