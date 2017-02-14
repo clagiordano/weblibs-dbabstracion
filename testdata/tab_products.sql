339a063f79be51a4022556a6378c69f2560fa169
Claudio Giordano <claudio.giordano@autistici.org>
2017-02-14 12:00:00 +0100

    Fix sql sample

--- testdata/tab_products.sql
+++ testdata/tab_products.sql
@@ -5,6 +5,6 @@ CREATE TABLE `tab_products` (
   `model` varchar(250) NOT NULL,
   `description` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='latin1_swedish_ci'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='latin1_swedish_ci';
 
 INSERT INTO `tab_products` set brand='brand', code='code', model='model', description='description';
